f34a11f14e336782a7eadbf0433e5c2b9eaa9961
cemsener <cemsener_@hotmail.com>
2024-01-25 02:33:20 +0300

    first commit

---
+++ server/routes/dalle.routes.js
@@ -0,0 +1,43 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import OpenAI from "openai";
+
+dotenv.config();
+
+const router = express.Router();
+
+const openai = new OpenAI(process.env.OPENAI_API_KEY);
+
+router.route("/").get((req, res) => {
+  res.status(200).json({ message: "Hello from DALL.E ROUTES" });
+});
+
+router.route("/").post(async (req, res) => {
+  try {
+    const { prompt } = req.body;
+
+    const response = await openai.createImage({
+      prompt,
+      n: 1,
+      size: "1024x1024",
+      response_format: "b64_json",
+    });
+
+    console.log(response);
+
+    const image = response.data.data[0].b64_json;
+
+    if (!image) {
+      console.error("Image data is undefined");
+    } else {
+      console.log("Image data:", image);
+    }
+
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
+export default router;
